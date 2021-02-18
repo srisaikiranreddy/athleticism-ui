379f10d1d006d574ea674bacbfa5c5b03d7eb94c
SaiKiran Palle <srisaikiranreddy@gmail.com>
2021-02-17 19:21:22 -0500

    ui design and full working changes

--- AthleticismUI/src/app/pages/home-page/home-page.component.ts
+++ AthleticismUI/src/app/pages/home-page/home-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
+import { ReviewService } from '../../shared/services/review.service';
+import { Review } from '../../shared/Models/review';
 
 @Component({
   selector: 'app-home-page',
@@ -9,83 +9,17 @@ import { Observable } from 'rxjs';
   styleUrls: ['./home-page.component.scss'],
 })
 export class HomePageComponent implements OnInit {
-  errorMessage: string
-  inputModel: any;
-  restItems: any;  
-  result$: Observable<any>;
-  private readonly  apiURL = 'https://localhost:5001/api/v1';
-
-
-  constructor(private http: HttpClient) {
-    this.result$ = this.resolveItems();
-  }
-
-  ngOnInit() {
-    // this.getRestItems();
-    // console.log(this.restItems);
-    // this.getCustomerById(1);
-    // this.result$ = this.resolveItems();
-
-    this.http.get<any>('https://localhost:5001/api/v1/Review').subscribe({
-      next: data => {
-          this.restItems = data;
-      },
-      error: error => {
-          this.errorMessage = error.message;
-          console.error('There was an error!', error);
-      }
-    })
-  }
-
-  // Read all REST Items
-  getRestItems() {
-    // return this.http
-    //   .get<any[]>(this.restItemsUrl,{observe:'response'})
-    //   .pipe(tap(data => data));
-  } 
-  
-  getCustomerById(id: number){
-    var test = this.http.get(`${this.apiURL}/Review/${id}`);
-    return test;
-  }
-
-  // URL which returns list of JSON items (API end-point URL)
-  
-
-  // create a method named: resolveItems()
-  // this method returns list-of-items in form of Observable
-  // every HTTTP call returns Observable object
-  resolveItems(): Observable<any> {
-    console.log('Request is sent!');
-    // this.http is a HttpClient library provide by @angular/common
-    // we are calling .get() method over this.http object
-    // this .get() method takes URL to call API
-    // return this.http.get(`${this.apiURL}/Review`);
-    // return this.http.get('https://localhost:5001/api/v1/Review').toPromise().then(res=> {
-    //   console.log(res.json();
-    //   resolve(););
-    // });
-
-    var promise = new Promise((resolve, reject) => {
-      var api = 'https://localhost:5001/api/v1/Review';
-      this.http.get(api)
-        .toPromise()
-        .then(
-          res => { // Success
-            var test = res;
-            resolve();
-          }
-        );
-    });
-    // return promise;
-
-    return this.http.get('https://localhost:5001/api/v1/Review');
+  postNewReview: boolean
+  reviewsData: any;
+  constructor(private reviewService:ReviewService) {
     
   }
 
-  resolveItemss(){
-    console.log('Request is sent!');
-    // return this.http.get<JSONObject>('https://localhost:5001/api/v1/Review');    
-  }
-
+  ngOnInit() {
+    this.postNewReview = false;
+    var test = this.reviewService.getReviews().subscribe(data =>{
+      this.reviewsData = data;
+    });
+  } 
+  
 }
--- AthleticismUI/src/app/shared/services/review.service.ts
+++ AthleticismUI/src/app/shared/services/review.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Review } from '../Models/review';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReviewService {
+  data: any;
+  private readonly  baseURL = 'https://localhost:5001/api/v1';
+  private readonly  reviewURL = `${this.baseURL}/review/`;
+  
 
-  constructor() { }
+  constructor(private http: HttpClient) {  }   
+
+  public createReview(review: Review){}
+
+  public updateReview(seview: Review){}
+
+  public deleteReview(id: number){}
+
+  public getReviewById(id: number){
+    return this.http.get<Review>(`${this.reviewURL}/${id}`)
+     .subscribe(data => {
+      this.data = data;
+    })
+  }
+
+  public getReviews(){
+    return this.http.get('https://localhost:5001/api/v1/review/')
+     
+  }
+
 }
